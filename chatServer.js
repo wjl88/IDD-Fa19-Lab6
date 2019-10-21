be2ea9053a05425a4f6ab53cbc0e7f463d1cbde1
wjl88 <wjl88@cornell.edu>
2019-10-21 14:50:50 -0700

    Changed If-stack to Switch-Case, added randomness
    Made a list randomly chosen phrases for when user says no to playing
    the chatbot game.

--- chatServer.js
+++ chatServer.js
@@ -1,7 +1,7 @@
 /*
-chatServer.js
-Author: David Goedicke (da.goedicke@gmail.com)
-Closley based on work from Nikolas Martelaro (nmartelaro@gmail.com) as well as Captain Anonymous (https://codepen.io/anon/pen/PEVYXz) who forked of an original work by Ian Tairea (https://codepen.io/mrtairea/pen/yJapwv)
+  chatServer.js
+  Author: David Goedicke (da.goedicke@gmail.com)
+  Closley based on work from Nikolas Martelaro (nmartelaro@gmail.com) as well as Captain Anonymous (https://codepen.io/anon/pen/PEVYXz) who forked of an original work by Ian Tairea (https://codepen.io/mrtairea/pen/yJapwv)
 */
 
 var express = require('express'); // web server application
@@ -10,6 +10,84 @@ var http = require('http').Server(app); // connects http library to server
 var io = require('socket.io')(http); // connect websocket library to server
 var serverPort = 8000;
 
+var PC_name = 'bil';
+var PC_HP = randomInt(4,13);
+var PC_creature = 'human';
+
+
+function randomInt(low, high) {
+    // Generate random integers between high and low
+    return Math.floor(Math.random() * (high - low) + low)
+}
+
+function randomColorBG(socket) {
+    // Select a random color to pass to the background
+    // ... calling random integer function
+    var red  = randomInt(100,200);
+    var green  = randomInt(100,200);
+    var blue = randomInt(100,200);
+    // create string RGB representation to pass along to socket
+    var c = 'rgb('+red+','+green+','+blue+')';
+    // send changeBG flag to socket with RGB string
+    socket.emit('changeBG', c);
+}
+
+function returnGameComment() {
+    var stringer = 'c\'mon';
+    switch ( randomInt(0,15) ) {
+    case 0 :
+	stringer = 'Seriously, pleeeaasseeee';
+	break;
+    case 1 :
+	stringer = 'But really, let\'s do it';
+	break;
+    case 2 :
+	stringer = 'I\'ll play fair, I promise';
+	break;
+    case 3 :
+	stringer = 'You know you want to';
+	break;
+    case 4 :
+	stringer = 'It\'ll be fun, I swear';
+	break;
+    case 5 :
+	stringer = 'It\'ll only take a minute';
+	break;
+    case 6 :
+	stringer = 'But c\'mon';
+	break;
+    case 7 :
+	stringer = 'You\'ll be great at it';
+	break;
+    case 8 :
+	stringer = 'Could you help me with this';
+	break;
+    case 9 :
+	stringer = 'All your friends are doing it';
+	break;
+    case 10 :
+	stringer = 'I won\'t tell anyone';
+	break;
+    case 11 :
+	stringer = 'I dare you';
+	break;
+    case 12 :
+	stringer = 'I double dog dare you';
+	break;
+    case 13 :
+	stringer = 'You don\'t have the gall';
+	break;
+    case 14 :
+	stringer = 'Betcha can\'t play';
+	break;
+    case 15 :
+	stringer = 'You\'re just being a chicken';
+	break;
+    }
+
+    stringer = stringer;
+    return stringer;
+}
 
 //---------------------- WEBAPP SERVER SETUP ---------------------------------//
 // use express to create the simple webapp
@@ -17,7 +95,7 @@ app.use(express.static('public')); // find pages in public directory
 
 // start the server and say what port it is on
 http.listen(serverPort, function() {
-  console.log('listening on *:%s', serverPort);
+    console.log('listening on *:%s', serverPort);
 });
 //----------------------------------------------------------------------------//
 
@@ -26,84 +104,102 @@ http.listen(serverPort, function() {
 // this is the websocket event handler and say if someone connects
 // as long as someone is connected, listen for messages
 io.on('connect', function(socket) {
-  console.log('a new user connected');
-  var questionNum = 0; // keep count of question, used for IF condition.
-  socket.on('loaded', function() { // we wait until the client has loaded and contacted us that it is ready to go.
-
-    socket.emit('answer', "Hey, hello I am \"___*-\" a simple chat bot example."); //We start with the introduction;
-    setTimeout(timedQuestion, 5000, socket, "What is your name?"); // Wait a moment and respond with a question.
-
-  });
-  socket.on('message', (data) => { // If we get a new message from the client we process it;
-    console.log(data);
-    questionNum = bot(data, socket, questionNum); // run the bot function with the new message
-  });
-  socket.on('disconnect', function() { // This function  gets called when the browser window gets closed
-    console.log('user disconnected');
-  });
+    console.log('a new user connected');
+    var questionNum = 0; // keep count of askQuestion, used for IF condition.
+    socket.on('loaded', function() { // we wait until the client has loaded and contacted us that it is ready to go.
+	socket.emit('answer', "Welcome to the Dungeon Bot"); //We start with the introduction;
+	setTimeout(timedQuestion, 1000, socket, "What's your name, kid?"); // Wait a moment and respond with a askQuestion.
+    });
+    socket.on('message', (data) => { // If we get a new message from the client we process it;
+	console.log(data);
+	questionNum = bot(data, socket, questionNum); // run the bot function with the new message
+    });
+    socket.on('disconnect', function() { // This function  gets called when the browser window gets closed
+	console.log('user disconnected');
+    });
 });
+
+
 //--------------------------CHAT BOT FUNCTION-------------------------------//
 function bot(data, socket, questionNum) {
-  var input = data; // This is generally really terrible from a security point of view ToDo avoid code injection
-  var answer;
-  var question;
-  var waitTime;
+    var input = data; // This is generally really terrible from a security point of view ToDo avoid code injection
+    var respondWith;
+    var askQuestion;
+    var waitTime;
+    var newQuestionNum;
+    randomColorBG(socket);
 
-  /// These are the main statments that make up the conversation.
-  if (questionNum == 0) {
-    answer = 'Hello ' + input + ' :-)'; // output response
-    waitTime = 5000;
-    question = 'How old are you?'; // load next question
-  } else if (questionNum == 1) {
-    answer = 'Really, ' + input + ' years old? So that means you were born in: ' + (2018 - parseInt(input)); // output response
-    waitTime = 5000;
-    question = 'Where do you live?'; // load next question
-  } else if (questionNum == 2) {
-    answer = 'Cool! I have never been to ' + input + '.';
-    waitTime = 5000;
-    question = 'Whats your favorite color?'; // load next question
-  } else if (questionNum == 3) {
-    answer = 'Ok, ' + input + ' it is.';
-    socket.emit('changeBG', input.toLowerCase());
-    waitTime = 5000;
-    question = 'Can you still read the font?'; // load next question
-  } else if (questionNum == 4) {
-    if (input.toLowerCase() === 'yes' || input === 1) {
-      answer = 'Perfect!';
-      waitTime = 5000;
-      question = 'Whats your favorite place?';
-    } else if (input.toLowerCase() === 'no' || input === 0) {
-      socket.emit('changeFont', 'white'); /// we really should look up the inverse of what we said befor.
-      answer = ''
-      question = 'How about now?';
-      waitTime = 0;
-      questionNum--; // Here we go back in the question number this can end up in a loop
-    } else {
-      question = 'Can you still read the font?'; // load next question
-      answer = 'I did not understand you. Could you please answer "yes" or "no"?'
-      questionNum--;
-      waitTime = 5000;
+    // Switch-case statements not if-else blocks
+    // ... The switch statements should allow for an expandable
+    // ... set of commands and responses.
+    switch ( questionNum ) {
+    case 0 :
+	PC_name = input;
+    	respondWith = 'Hello, ' + PC_name; // output response
+    	askQuestion = 'Have you ever played Dungeons and Dragons?'; // load next askQuestion
+    	waitTime = 1000;
+	newQuestionNum = 1;
+    	break;
+    case 1 :
+    	respondWith = 'I\'m not sure what ' + input + ' means, but cool'; // output response
+    	askQuestion = 'What character or creature do you want to be?'; // load next askQuestion
+	waitTime = 1000;
+	newQuestionNum = 2;
+    	break;
+    case 2 :
+	PC_creature = input;
+    	respondWith = 'Great, you be a ' + input + ' and I\'ll be a dragon!'; // output response
+    	askQuestion = 'Do you want to play? yes or no'; // load next askQuestion
+	waitTime = 1500;
+	newQuestionNum = 3;
+    	break;
+    case 3 :
+	switch ( input.toLowerCase() ) {
+	case ( 'no' ) :
+	    respondWith = returnGameComment(); // output response
+    	    askQuestion = returnGameComment() + ', ' + PC_name + '\r\n(yes or no)'; // load next askQuestion
+	    newQuestionNum = 3;
+	    waitTime = 1000;
+	    break;
+	case ( 'yes' ) :
+	    respondWith = 'Excellent, ' + input + '!'; // output response
+    	    askQuestion = 'What action would you like to take against my dragon?'; // load next askQuestion
+	    newQuestionNum = 4;
+	    waitTime = 1000;
+	    break;
+	default :
+	    respondWith = '\"' + input + '\" doesn\'t make any sense, ' + PC_name; // output response
+    	    askQuestion = 'Are you down to play or not? yes or no'; // load next askQuestion
+	    waitTime = 1000;
+	    newQuestionNum = 3;
+	    break;
+	}
+	break;
+    case 4 :
+    	respondWith = 'You try ' + input + '\n... it\'s not very effective ... '; // output response
+    	askQuestion = ''; // prevent questions from loading
+	waitTime = 1000;
+	newQuestionNum = 5;
+    	break;
+    case 5 :
+	respondWith = 'My dragon attacks your ' + PC_creature + ' reducing it\'s ' + PC_HP + ' Hit Points to zero... you died.' ; // output response
+    	askQuestion = ''; // prevent questions from loading
+	waitTime = 1000;
+    	break;
     }
-    // load next question
-  } else {
-    answer = 'I have nothing more to say!'; // output response
-    waitTime = 0;
-    question = '';
-  }
 
-
-  /// We take the changed data and distribute it across the required objects.
-  socket.emit('answer', answer);
-  setTimeout(timedQuestion, waitTime, socket, question);
-  return (questionNum + 1);
+    /// We take the changed data and distribute it across the required objects.
+    socket.emit('answer', respondWith);
+    setTimeout(timedQuestion, waitTime, socket, askQuestion);
+    return (newQuestionNum);
 }
 
-function timedQuestion(socket, question) {
-  if (question != '') {
-    socket.emit('question', question);
-  } else {
-    //console.log('No Question send!');
-  }
+function timedQuestion(socket, askQuestion) {
+    if (askQuestion != '') {
+	socket.emit('question', askQuestion);
+    } else {
+	//console.log('No Question send!');
+    }
 
 }
 //----------------------------------------------------------------------------//
